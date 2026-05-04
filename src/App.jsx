edbe4af2b886f3d19b2ba3d104c726d4859d96c5
natethedog04-afk <natethedog04@gmail.com>
2026-05-04 19:44:03 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navigation from './components/Navigation'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import ProjectDetail from './pages/ProjectDetail'
+import Contact from './pages/Contact'
+import './App.css'
+
+function App() {
+  return (
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/projects/:id" element={<ProjectDetail />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
